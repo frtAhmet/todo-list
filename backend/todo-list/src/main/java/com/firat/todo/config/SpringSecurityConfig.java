e80418d097a0ef3c9ae3e5d7808264e14bd21237
Ahmet FIRAT <ahmet.firat.cmpe@gmail.com>
2024-07-29 23:30:23 +0300

    Update SpringSecurityConfig.java

--- backend/todo-list/src/main/java/com/firat/todo/config/SpringSecurityConfig.java
+++ backend/todo-list/src/main/java/com/firat/todo/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.firat.todo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,6 +27,12 @@ public class SpringSecurityConfig {
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
+                    authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN", "USER");
+                    // authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll(); // public
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
 
